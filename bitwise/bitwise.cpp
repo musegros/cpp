dabe7ef79ef61ac0b832125f356474ab1958b14a
Josh Musgrave <joshmusgrave123@gmail.com>
2021-03-16 11:37:26 -0500

    added rotation.cpp

--- bitwise/bitwise.cpp
+++ bitwise/bitwise.cpp
@@ -1,6 +1,9 @@
 #include "printBinary.h"
 #include <iostream>
 
+unsigned char rol(unsigned char);
+unsigned char ror(unsigned char);
+
 using namespace std;
 
 #define PR(STR, EXPR) \
@@ -20,6 +23,10 @@ int main() {
 	PR("a ^ b = ", a ^ b);
 	PR("~a = ", ~a);
 	PR("~b = ", ~b);
+	PR("a before rotate = ", a);
+	PR("b before rotate = ", b);
+	PR("ror(a) = ", ror(a));
+	PR("rol(b) = ", rol(b));
 	unsigned char c = 0x5A;
 	PR("c in binary: ", c);
 	a |= c;
